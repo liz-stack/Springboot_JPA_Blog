0b91d3368b23fd9d3a556188e900643a97e356b8
liz-stack <yeoju.kook@gmail.com>
2022-03-08 21:50:31 +0900

    lombok, maven 실습

--- src/main/java/com/cos/blog/test/HttpController.java
+++ src/main/java/com/cos/blog/test/HttpController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //사용자가 요청 -> 응답(HTML파일)
@@ -14,24 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpController {
 	
+	private static final String TAG = "HttpController Test: ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//Member m = new Member(1, "liz", "1234", "liz@naver.com");
+		Member m = Member.builder().username("liz").password("1234").email("liz@naver.com").build();
+		System.out.println(TAG+"getter: "+ m.getUsername());
+		m.setUsername("liz1");
+		System.out.println(TAG+"getter: "+ m.getUsername());
+		return "lombok test 완료";
+	}
+	
 	// 인터넷 브라우저 요청은 get방식만 가능!
 	//http://localhost:8080/http/get
 	@GetMapping("/http/get")
-	public String getTest(Member m) {	
+	public String getTest(Member m) {//id=1&username=ssar&password=1234&email=ssar@naver.com
 	//public String getTest(@RequestParam int id, @RequestParam String username) {
 		return "get 요청: "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 
 	//http://localhost:8080/http/post
-	@PostMapping("/http/post")
-	public String postTest() {
-		return "post요청";
+	@PostMapping("/http/post") //text/plain, application/json
+	public String postTest(@RequestBody Member m) { //MessageConverter가 자동으로 파싱해서 오브젝트에 넣어줌
+		//parsing : 다른 언어로 작성된 문서를 디코딩(문법적으로 해체)
+		return "post 요청: "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 	
 	//http://localhost:8080/http/put
 	@PutMapping("http/put")
-	public String putTest() {
-		return "put요청";
+	public String putTest(@RequestBody Member m) {
+		return "put요청"+m.getId()+", "+m.getPassword()+", ";
 	}
 	
 	//http://localhost:8080/http/delete
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,42 +1,33 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
+	//@AllArgsConstructor //모든 필드를 다 쓰는 생성자
+	//@NoArgsconstructor //파라미터가 없는 기본 생성자(빈생성자)
+	//@RequiredArgsConstructor //final한 필드에 대한 생성자 만들어줌
 	
-	
+	@Builder
 	public Member(int id, String username, String password, String email) {
 		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
-	}
+	} 
+	//@Builder사용시 빌더 패턴을 따로 만들 필요없게됨.& 인자 순서 상관x
+
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
