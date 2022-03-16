c8c2b043b78f60515ed8e9e4b1c3a5405aeaba50
liz-stack <yeoju.kook@gmail.com>
2022-03-16 22:09:09 +0900

    로그인, 회원가입 화면구현(controller, view단 만들기 시작)

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(value=Exception.class)
+	@ExceptionHandler(value = Exception.class)
 	public String handleArgumentException(IllegalArgumentException e) {
-		return "<h1>"+e.getMessage()+"</h1>";
+		return "<h1>" + e.getMessage() + "</h1>";
 	}
 }
